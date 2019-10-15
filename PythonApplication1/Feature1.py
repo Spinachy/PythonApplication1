4e4f54d277f166da4a9db652b6f474b1ef8884eb
Tony Mollentz <tony@mollentz.com>
2019-10-15 18:09:34 +0200

    Now added exciting feature called "Feature1"

---
+++ PythonApplication1/Feature1.py
@@ -0,0 +1,6 @@
+class Feature1(object):
+    """description of Feature1"""
+
+
+
+
--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,6 +3,8 @@ print('Hello');
 print('Kilroy Was Here')
 # This comment was added in VS
 # Added a 4th line ....
+
+# now calling Feature1
 print('Fourth Line')
 print('Bye Bye')
 
